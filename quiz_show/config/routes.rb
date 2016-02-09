ca5565b7055e98025a7c2ec9dc7fe3c4573a62db
Willis Lau <lauwillis@yahoo.com>
2016-02-09 15:31:22 -0500

    added more features for quiz

--- quiz_show/config/routes.rb
+++ quiz_show/config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'categories/show'
 
 
-  get 'users/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -15,7 +14,6 @@ Rails.application.routes.draw do
   root 'static#home'
   get '/' => 'sessions#new'
   post '/' => 'sessions#create'
- 
   delete '/logout' => 'sessions#destroy'
 
   # Example of regular route:
