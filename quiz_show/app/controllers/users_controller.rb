e0e9b8d31d95be36bf30074bf7c73bbd0b8714e9
Mike Puzio <mcpuzio@gmail.com>
2016-02-09 19:59:39 -0500

    Refined the stylings; still needs work

--- quiz_show/app/controllers/users_controller.rb
+++ quiz_show/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   		session[:user_id] = @user.id
-  		redirect_to current_user
+  		redirect_to plans_path
   	else
         redirect_to :back
   	end
